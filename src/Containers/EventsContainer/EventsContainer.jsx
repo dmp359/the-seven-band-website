e74af82384327ab80fca1c99935e32f3455ac17c
Dan Perlman <dmp359@drexel.edu>
2019-10-13 16:42:09 -0400

    comments

--- src/Containers/EventsContainer/EventsContainer.jsx
+++ src/Containers/EventsContainer/EventsContainer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import EventCard from 'Components/EventCard';
 
+// TODO: Make this an actual grid
 function EventsContainer({ events }) {
   return (
-  <grid>
-  {events.map((event) => (
-    <EventCard {...event} />
-    )
-  )}
-  </grid>
+    <grid>
+      {events.map((event) => (
+        <EventCard {...event} />
+        )
+      )}
+    </grid>
   )
 }
 
